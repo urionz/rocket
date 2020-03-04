bcee75509aa9911e3012600988c5929e02ac3d00
root <root@zhangyuren-dev1.novalocal>
2020-03-04 18:53:39 +0800

    update build

---
+++ deploy/docker/rocket/build/.bundle-garbage-18761ae.y3no/programs/web.browser.legacy/dynamic/app/integrations/client/streamer.js
@@ -0,0 +1,2 @@
+function module(e,r,t){var n;t.export({integrationHistoryStreamer:function(){return o}}),t.link("meteor/meteor",{Meteor:function(e){n=e}},0);var o=new n.Streamer("integrationHistory")}
+
---
+++ deploy/docker/rocket/build/.bundle-garbage-18761ae.y3no/programs/web.browser.legacy/dynamic/client/components/basic/RawText.js
@@ -0,0 +1,2 @@
+function module(n,e,t){var r;t.export({RawText:function(){return a}}),t.link("react",{default:function(n){r=n}},0);var a=function(n){var e=n.children;return r.createElement("span",{dangerouslySetInnerHTML:{__html:e}})}}
+
